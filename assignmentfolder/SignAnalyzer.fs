fc0d0cafd069294b3752b80003434927ea89967f
Joel Madsen <joel.madsen10@gmail.com>
2021-04-09 10:38:18 +0200

    Added sign regex checks

--- assignmentfolder/SignAnalyzer.fs
+++ assignmentfolder/SignAnalyzer.fs
@@ -1,5 +1,41 @@
 module SignAnalyzer
 
+open System.Text.RegularExpressions
+
+// From: https://stackoverflow.com/questions/53818476/f-match-many-regex
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
+
+// Check if a string is a sign type
+let signChecker s =
+    match s with
+    | Regex @"(^\+)" [ chr ]    -> Plus
+    | Regex @"(^\-)" [ chr ]    -> Minus
+    | Regex @"(^0)"  [ chr ]    -> Zero
+    | _                         -> printfn "Not a valid sign initialization"
+                                   failwith "Not a valid sign initialization"
+
+// Check if a sign array is on the correct form
+let signArrayChecker (a:string) =
+    match a with
+    | Regex @"^\{((\s*[\+ \- 0]+\s*,?\s*)+\s*[\+ \- 0]*\s*)\}" [ num; d ] ->
+        num
+    | _ ->  printfn "ERROR: Array failed to hold correct syntax of \"{int 1, int 2, ..., int N}\""
+            failwith "Array failed to hold correct syntax of \"{int 1, int 2, ..., int N}\""
+
+// Convert a string list to a signs list
+let arrayStringToSignList (s:string) =
+    let sA = Seq.toList (signArrayChecker s)
+    let rec arrayStringToListS (l:char List) (nl:Signs List) =
+        match l with
+        | [] -> nl
+        | (x:char)::[] -> arrayStringToListS [] (nl@[signChecker (string x)])
+        | (x:char)::tail when x = ',' -> arrayStringToListS tail (nl)
+        | (x:char)::tail -> arrayStringToListS tail (nl@[signChecker (string x)])
+
+    arrayStringToListS sA []
 
 
 let rec signAnalysis (edges:(int * SubTypes * int)List) (edgestail:(int * SubTypes * int)List) (varList:(AExpr * (int)List) List) (q:int) =
